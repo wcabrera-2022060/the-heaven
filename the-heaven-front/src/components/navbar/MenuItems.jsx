03e269347698b8a2b4e0dfefa1436dcf88c98fc6
mcastanon-2019078 <mcastanon-2019078@kinal.edu.gt>
2024-05-22 14:11:55 -0600

    Proyecto Realizado Al 90%

--- the-heaven-front/src/components/navbar/MenuItems.jsx
+++ the-heaven-front/src/components/navbar/MenuItems.jsx
@@ -17,15 +17,42 @@ export const MenuItems = [
         cName: 'nav-links',
         icon: 'fa-solid fa-briefcase',
     },
+    {
+        title: 'Admin',
+        cName: 'nav-links',
+        icon: 'fa-solid fa-user',
+        subItems: [
+            {
+                title: 'Hotel',
+                url: '/hotelA',
+                cName: 'nav-links btn',
+            },
+            {
+                title: 'Reservation',
+                url: '/reservationA',
+                cName: 'nav-links btn',
+            },
+            {
+                title: 'Room',
+                url: '/roomA',
+                cName: 'nav-links btn',
+            },
+            {
+                title: 'Service',
+                url: '/serviceA',
+                cName: 'nav-links btn',
+            },
 
+            {
+                title: 'Event',
+                url: '/EventA',
+                cName: 'nav-links btn',
+            },
+        ],
+    },
     {
         title: 'Sign in',
         url: '/signin',
         cName: 'nav-links btn',
-    },
-    {
-        title: 'Sing up',
-        url: '/singup',
-        cName: 'nav-links btn2',
     },
 ]
--- the-heaven-front/src/components/navbar/Navbar.jsx
+++ the-heaven-front/src/components/navbar/Navbar.jsx
@@ -1,31 +1,55 @@
 import { useState } from 'react'
 import './Navbar.css'
-import { MenuItems } from './MenuItems'
 import { Link } from 'react-router-dom'
+import { MenuItems } from './MenuItems'
 
 export const Navbar = () => {
-  const [clicked, setClicked] = useState(false)
+    const [clicked, setClicked] = useState(false)
 
-  const handleClick = () => {
-    setClicked(!clicked)
-  }
+    const handleClick = () => {
+        setClicked(!clicked)
+    }
 
-  return (
-    <nav className="NavbarItems">
-      <p className="navbar-logo">The Heaven</p>
-      <div className="menu-icons" onClick={handleClick}>
-        <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-      </div>
-      <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
-        {MenuItems.map((item, index) => (
-          <li key={index}>
-            <Link className={item.cName} to={item.url}>
-              {item.title}
-              <i className={item.icon}></i>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  )
+    return (
+        <nav className="NavbarItems">
+            <p className="navbar-logo">The Heaven</p>
+            <div className="menu-icons" onClick={handleClick}>
+                <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+            </div>
+            <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
+                {MenuItems.map((item, index) => (
+                    <li key={index}>
+                        {item.subItems ? (
+                            <>
+                                <span
+                                    className={item.cName}
+                                    onClick={handleClick}
+                                >
+                                    {item.title}
+                                    <i className={item.icon}></i>
+                                </span>
+                                <ul className="sub-menu">
+                                    {item.subItems.map((subItem, subIndex) => (
+                                        <li key={subIndex}>
+                                            <Link
+                                                className={subItem.cName}
+                                                to={subItem.url}
+                                            >
+                                                {subItem.title}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </>
+                        ) : (
+                            <Link className={item.cName} to={item.url}>
+                                {item.title}
+                                <i className={item.icon}></i>
+                            </Link>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
 }
